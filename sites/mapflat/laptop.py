6a781f718a6e2ce599e645f51a6ceb404e06e4a3
Lars Albertsson <lalle@mapflat.com>
2016-05-26 23:58:41 +0200

    Add support for add-apt-repository.

--- sites/mapflat/laptop.py
+++ sites/mapflat/laptop.py
@@ -17,6 +17,22 @@ apt.KeyAdd("https://packages.cloud.google.com/apt/doc/apt-key.gpg")
 apt.SourceList("spotify", "deb http://repository.spotify.com stable non-free")
 apt.KeyRecv("hkp://keyserver.ubuntu.com:80", "BBEBDCB318AD50EC6865090613B00F1FD2C19886")
 
+apt.SourceList("google-chrome", "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main")
+apt.KeyAdd("https://dl.google.com/linux/linux_signing_key.pub")
+
+apt.SourceList("samsung", "deb http://www.bchemnet.com/suldr/ debian extra")
+apt.KeyAdd("http://www.bchemnet.com/suldr/suldr.gpg")
+
+apt.KeyRecv("hkp://p80.pool.sks-keyservers.net:80", "58118E89F3A912897C070ADBF76221572C52609D")
+apt.SourceList("docker",
+               content.OutputOf('echo "deb https://apt.dockerproject.org/repo ubuntu-$(lsb_release -c -s) main"',
+                                shell=True))
+
+apt.KeyAdd("https://download.01.org/gfx/RPM-GPG-KEY-ilg-3")
+
+apt.SourceList("sbt", "deb https://dl.bintray.com/sbt/debian /")
+apt.KeyRecv("hkp://keyserver.ubuntu.com:80", "642AC823")
+
 
 # For gsutil
 apt.Install(['libffi-dev', 'libssl-dev'], update_first=True)
@@ -26,12 +42,19 @@ apt.Install("google-cloud-sdk")
 
 apt.Install("tox")
 
+# Development
+apt.Install("python3-pip")
+
+pip.Install("awscli")
+apt.Install("jq")
+pip.Install("restview")
+
 debconf.SetSelections('ttf-mscorefonts-installer', 'msttcorefonts/accepted-mscorefonts-eula',
                       'true')
 
-apt.Install("kubuntu-desktop")
-
 apt.Install('ubuntu-session')
+
+apt.Install("kubuntu-desktop")
 
 apt.Install("konqueror")
 apt.Install("spotify-client")
@@ -41,15 +64,6 @@ apt.Install("acpi")
 
 # Needed for Intel graphics installer
 apt.Install("ttf-ancient-fonts")
-
-apt.KeyAdd("https://download.01.org/gfx/RPM-GPG-KEY-ilg-3")
-
-# Development
-apt.Install("python3-pip")
-
-pip.Install("awscli")
-apt.Install("jq")
-pip.Install("restview")
 
 files.Transform("/usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf",
                 lambda c: re.sub(r"user-session=.*", "user-session=kde-plasma", c))
