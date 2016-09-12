866e5151948603396efe8de777732c321b9b355f
Lars Albertsson <lalle@mapflat.com>
2016-09-12 20:31:56 +0200

    Add multiline test.

--- tests/test_files.py
+++ tests/test_files.py
@@ -3,7 +3,6 @@ import sys
 
 import re
 
-
 sys.path.append("{}/..".format(os.path.dirname(__file__)))
 
 import fixture
@@ -13,19 +12,22 @@ class FileChmodTest(fixture.DockerTest):
     def test_chmod_600(self):
         self.stuff(['files.Chmod(0o600, "/usr/share/common-licenses/GPL-2")'])
         self.assertTrue(re.search(r"-rw------- ",
-                                  self.container_run(["ls", "-l", "/usr/share/common-licenses/GPL-2"])))
+                                  self.container_run(
+                                      ["ls", "-l", "/usr/share/common-licenses/GPL-2"])))
 
 
 class FileChownTest(fixture.DockerTest):
     def test_chown(self):
         self.stuff(['files.Chown("games", "/usr/share/common-licenses/GPL-2")'])
         self.assertTrue(re.search(r" games ",
-                                  self.container_run(["ls", "-l", "/usr/share/common-licenses/GPL-2"])))
+                                  self.container_run(
+                                      ["ls", "-l", "/usr/share/common-licenses/GPL-2"])))
 
     def test_chown_r(self):
         self.stuff(['files.Chown("games", "/usr/share/common-licenses", recursive=True)'])
         self.assertTrue(re.search(r" games ",
-                                  self.container_run(["ls", "-l", "/usr/share/common-licenses/GPL-3"])))
+                                  self.container_run(
+                                      ["ls", "-l", "/usr/share/common-licenses/GPL-3"])))
 
 
 class FileContentTest(fixture.DockerTest):
@@ -58,8 +60,9 @@ class FileDownloadTest(fixture.DockerTest):
         self.stuff(['files.DownloadFile("https://github.com/docker/compose/releases/download/1.6.2/'
                     'docker-compose-Linux-x86_64", '
                     '"/usr/local/bin/docker-compose")'])
-        self.assertEquals(self.container_run(["shasum", "/usr/local/bin/docker-compose"]).split()[0],
-                          "82129d4e90c7544a52738a570747d653bdb656db")
+        self.assertEquals(
+            self.container_run(["shasum", "/usr/local/bin/docker-compose"]).split()[0],
+            "82129d4e90c7544a52738a570747d653bdb656db")
 
 
 class FileMkdirTest(fixture.DockerTest):
@@ -72,4 +75,10 @@ class FileTransformTest(fixture.DockerTest):
     def test_basic(self):
         self.stuff(['files.Content("/tmp/transform_test", "old_content\\n")'])
         self.stuff(['files.Transform("/tmp/transform_test", lambda c: c.replace("old", "new"))'])
+        self.assertEquals(self.container_run(["cat", "/tmp/transform_test"]), "new_content\n")
+
+    def test_multiline(self):
+        """Test that multiline scripts work"""
+        self.stuff(['files.Content("/tmp/transform_test", "old_content\\n")',
+                    'files.Transform("/tmp/transform_test", lambda c: c.replace("old", "new"))'])
         self.assertEquals(self.container_run(["cat", "/tmp/transform_test"]), "new_content\n")
